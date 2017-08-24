891c236776eef20eb482be20b0fe939a27e61768
shaonianzhentan <shaonianzhentan@qq.com>
2017-08-24 17:30:24 +0800

    提交命令

---
+++ chrome.cmd
@@ -0,0 +1,3 @@
+cd C:\Program Files (x86)\Google\Chrome\Application
+chrome.exe --app="http://localhost:8888/index.html" --kiosk --args --disable-web-security --disable-desktop-notifications --disable-popup-blocking --user-data-dir
+pause
